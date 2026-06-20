b64486faa1696058376440f72c200d8ee387096f
umar <umarsa8080@gmail.com>
2026-06-20 14:22:46 +0100

    use virtual environment in build_files.sh to bypass PEP 668

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+# Create and activate virtual environment to bypass PEP 668 externally-managed-environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install requirements inside virtualenv
 pip install -r requirements.txt
+
+# Run collectstatic to generate staticfiles directory
 python3 manage.py collectstatic --noinput
